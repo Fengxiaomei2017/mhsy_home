af617a365c7663a753d9721ed578aa8c8f5737f5
fengxiaomei <fengxiaomei@mhsy.com>
2017-12-04 19:09:44 +0800

    全局引入

--- src/main.js
+++ src/main.js
@@ -1,15 +1,43 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from '@/App'
+import router from '@/router'
+// import vuex from 'vuex'
+// import axios from 'axios'
 
+// import iView from 'iview'
+import '@/common/css/reset.css'
+import '@/common/css/icon.css'
+
+// import {
+//   GAIBIAN_MOKUAI,
+//   CHAXUN_LEIXING
+// } from '@/common/js/changliang.js'
+// Vue.use(vuex)
+// Vue.use(iView)
+
+// Vue.prototype.$http = axios
 Vue.config.productionTip = false
-
+// const vuexstore = new vuex.Store({
+//   state: {
+//     mokuai: '',
+//     chaxunleixing: ''
+//   },
+//   mutations: {
+//     [GAIBIAN_MOKUAI] (state, mokuai) {
+//       state.mokuai = mokuai
+//     },
+//     [CHAXUN_LEIXING] (state, chaxunleixing) {
+//       state.chaxunleixing = chaxunleixing
+//     }
+//   }
+// })
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  router: router,
+  // store: vuexstore,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,44 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Shouye from '@/components/gwshouye/Gwshouye.vue'
+import Qiyejies from '@/components/gwqiyejies/Gwqiyejies.vue'
+import Gwxinwen from '@/components/gwxinwen/Gwxinwen.vue'
+import Gwxwquanbu from '@/components/gwxwquanbu/Gwxwquanbu.vue'
+import Gwdaohangmoban from '@/components/gwdaohangmoban/Gwdaohangmoban.vue'
+import Gwlianxiwomen from '@/components/gwlianxiwomen/Gwlianxiwomen.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'default',
+      redirect: '/shouye'
+    },
+    {
+      path: '/shouye',
+      component: Shouye
+    },
+    {
+      path: '/qiyejies',
+      component: Qiyejies
+    },
+    {
+      path: '/gwxinwen',
+      component: Gwxinwen
+    },
+    {
+      path: '/gwxwquanbu',
+      component: Gwxwquanbu
+    },
+    {
+      path: '/lianxiwomen',
+      component: Gwlianxiwomen
+    },
+    {
+      path: '/Gwdaohangmoban/:Bianma',
+      component: Gwdaohangmoban
     }
   ]
 })
